7e5af4003bde07951a2bc1b499b519f1583235c1
Shrikiran11 <92041814+Shrikiran11@users.noreply.github.com>
2022-09-05 21:48:13 +0530

    CommandLine_2.2_.java

---
+++ CommandLine.java
@@ -0,0 +1,9 @@
+public class CommandLine{
+public static void main(String args[]){
+int number=Integer.parseInt(args[0]);
+if(number>=0)
+System.out.println("Positive");
+else
+System.out.println("Negative");
+}
+}
